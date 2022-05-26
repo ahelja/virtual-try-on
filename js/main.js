93b36b578db5582265035008c9efe02c8fb84f34
Arnel Helja <ahelja@alpenite.com>
2022-05-26 17:52:50 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,22 @@
 
 $(document).foundation();
+
+const ctaVto = document.querySelectorAll('.flip-card');
+
+function removeNavClass() {
+	for (let i = 0; i < ctaVto.length; i++){
+		ctaVto[i].classList.remove('active');
+	}
+};
+
+function addCustomEventListener() {
+	for (let i = 0; i < ctaVto.length; i++){
+		ctaVto[i].addEventListener('click', function(e) {
+			removeNavClass();
+			e.currentTarget.classList.add('active');
+		});
+	}
+};
+
+removeNavClass();
+addCustomEventListener();
